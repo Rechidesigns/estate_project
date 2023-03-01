24aa918fc9899ac2b7679754241f976c99f8d780
Rechidesigns <alerechiordu@gmail.com>
2023-03-02 00:17:22 +0100

    url bug fixing

--- properties/api/urls.py
+++ properties/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('property-type-options', views.Property_Options_ViewSet.as_view() , name = 'property_type_options'),
+    path('property-type-options/', views.Property_Options_ViewSet.as_view() , name = 'property_type_options'),
     path('landlord-properties/' , views.Properties_View.as_view() , name = "properties_view"),
 ]
